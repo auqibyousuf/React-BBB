e374120185ea97261d749d32832c46c51d038949
auqibyousuf <auqib.yousuf@axelerant.com>
2023-02-11 21:03:20 +0530

    Add Tailwind support to storybook.

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { ImageTypes } from "../../types/image.types";
 const Image = ({imgUrl, altText, extraClasses}:ImageTypes) => {
   return (
+    <>
         <img src={imgUrl} alt={altText} className={extraClasses} />
+    </>
   );
 }
 
