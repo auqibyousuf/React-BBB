1e8c112fc9d444e83c06e46ed66256e8875f0ebb
auqibyousuf <auqib.yousuf@axelerant.com>
2023-02-13 16:49:52 +0530

    fix tailwind path issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
-  content: ["./src/*.{html,js,tsx}"],
+  content: ["./src/components/**/*.{html,js,tsx}","./src/*.{html,js,tsx}"],
   theme: {
     extend: {},
   },
