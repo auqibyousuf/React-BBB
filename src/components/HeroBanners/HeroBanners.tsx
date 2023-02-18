13f5af6f57bee60f6f3a23a6a47ae0ba6b1e30a2
auqibyousuf <auqib.yousuf@axelerant.com>
2023-02-18 11:02:32 +0530

    Added HeroSection stories.

--- src/components/HeroBanners/HeroBanners.tsx
+++ src/components/HeroBanners/HeroBanners.tsx
@@ -26,9 +26,15 @@ export const HeroBanners = ({
     {
       "flex-row-reverse sm:flex-row-reverse": imageAlignment == "image-left",
       "lg:pl-24 md:pl-14 sm:pl-8 bg-core-primary text-white":
-        variant == "image text with button",
-      "lg:pr-37 md:pr-14 sm:pr-8 pr-4 text-image-with-two-buttons bg-background-neutral-medium items-center":
-        variant == "image text with two buttons",
+        variant == "image text with button" && imageAlignment == "image-left",
+      "lg:pr-24 md:pr-14 sm:pr-8":
+        variant == "image text with button" && imageAlignment == "image-right",
+      "lg:pr-[148px] md:pr-14 sm:pr-8 pr-4 text-image-with-two-buttons bg-background-neutral-medium items-center":
+        variant == "image text with two buttons" &&
+        imageAlignment == "image-left",
+      "lg:pl-[148px] md:pl-14 sm:pl-8 pl-4":
+        variant == "image text with two buttons" &&
+        imageAlignment == "image-right",
     },
     extraClasses
   );
@@ -41,7 +47,7 @@ export const HeroBanners = ({
   const txtWrapperClasses = classNames({
     "lg:py-[260px] lg:w-[500px] md:py-[155px] md:w-[444px] sm:py-16 sm:w-[340px] xs:pl-4 sm:pl-0 xs:pb-12 sm:pb-0  sm:mt-0":
       variant == "image text with button",
-    "md:py-[81px] sm:py-16 lg:py-60 xs:py-0  lg:max-w-[547px] md:max-w-[444px] sm:max-w-[340px] xs:mt-[98px] xs:px-4 sm:px-0":
+    "md:py-[81px] sm:py-16 lg:py-60 xs:py-0  lg:max-w-[547px] md:max-w-[444px] sm:max-w-[340px] xs:mt-[98px] xs:px-4 sm:px-0 ":
       variant == "image text with two buttons",
   });
   return (
@@ -58,18 +64,24 @@ export const HeroBanners = ({
           variant={paragraphVariant}
           extraClasses="lg:mt-6 lg:mb-12 sm:my-6 xs:mt-4 xs:mb-6"
         />
-        <Button
-          variant={btn1Variant}
-          btnText={btnText}
-          rightIcon={MdArrowForwardIos}
-        />
-        {btn2Text && (
-          <Button
-            variant={btn2Variant}
-            btnText={btn2Text}
-            rightIcon={MdArrowForwardIos}
-          />
-        )}
+        <ul className="flex lg:flex-row flex-col">
+          <li>
+            <Button
+              variant={btn1Variant}
+              btnText={btnText}
+              rightIcon={MdArrowForwardIos}
+            />
+          </li>
+          <li>
+            {btn2Text && (
+              <Button
+                variant={btn2Variant}
+                btnText={btn2Text}
+                rightIcon={MdArrowForwardIos}
+              />
+            )}
+          </li>
+        </ul>
       </div>
       <div>
         <Image extraClasses={imageClasses} imgUrl={imageUrl} altText="" />
